b1bce2ea95b96a231b1853b3d2b034d7ac505597
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-06 14:55:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,12 @@
 class Application
    attr_accessor :items
- 
+
    def call(env)
      resp = Rack::Response.new
      req = Rack::Request.new(env)
- 
+
      if req.path.match(/items/)
- 
+
        search_item = req.path.split("/items/").last
        if item = @@items.find{|i| i.name == search_item}
          resp.write item.price
@@ -17,11 +17,9 @@ class Application
      else
        resp.write "Route not found"
        resp.status = 404
- 
+
    end
- 
+
      resp.finish
    end
  end
-
-  
